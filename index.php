5cffbe5ce5c135053c03583f87b928cb8c2851ef
SansGodasse <sansgodasse@gmail.com>
2019-02-13 20:34:51 +0100

    Add some CSS transitions

--- index.php
+++ index.php
@@ -19,39 +19,45 @@
         </header>
 
         <section class="content">
-            <h3>La formation OpenClassrooms</h3>
+            <div class="box">
+                <h3>La formation OpenClassrooms</h3>
 
-            <div class="card-deck">
-                <a class="card" href="https://openclassrooms.com/fr/paths/59-developpeur-dapplication-php-symfony">Développeur d'application php/symfony</a>
+                <div class="card-deck">
+                    <a class="card" href="https://openclassrooms.com/fr/paths/59-developpeur-dapplication-php-symfony">Développeur d'application php/symfony</a>
+                </div>
             </div>
 
-            <h3>Mes projets liés à la formation</h3>
+            <div class="box">
+                <h3>Mes projets liés à la formation</h3>
 
-            <h4>Projets réalisés</h4>
+                <h4>Projets réalisés</h4>
 
-            <div class="card-deck">                
-                <a class="card" href="https://formation-oc-php.sansgodasses.com/projet-2/">Intégrez un thème Wordpress pour un client</a>
+                <div class="card-deck">                
+                    <a class="card" href="https://formation-oc-php.sansgodasses.com/projet-2/">Intégrez un thème Wordpress pour un client</a>
 
-                <a class="card" href="https://formation-oc-php.sansgodasses.com/projet-3/">Analysez les besoins de votre client pour son Festival de films</a>
+                    <a class="card" href="https://formation-oc-php.sansgodasses.com/projet-3/">Analysez les besoins de votre client pour son Festival de films</a>
 
-                <a class="card" href="https://formation-oc-php.sansgodasses.com/projet-4/">Concevez la solution technique d'une application de restauration en ligne, Express Food</a>
+                    <a class="card" href="https://formation-oc-php.sansgodasses.com/projet-4/">Concevez la solution technique d'une application de restauration en ligne, Express Food</a>
+                </div>
+
+                <h4>Projets à venir...</h4>
+
+                <ul>
+                    <li>Créez votre premier blog en PHP</li>
+                    <li>Développez de A à Z le site communautaire SnowTricks</li>
+                    <li>Créez un web service exposant une API</li>
+                    <li>Améliorez une application existante de ToDo & Co</li>
+                </ul>
             </div>
 
-            <h4>Projets à venir...</h4>
+            <div class="box">
+                <h3>Mes projets perso</h3>
 
-            <ul>
-                <li>Créez votre premier blog en PHP</li>
-                <li>Développez de A à Z le site communautaire SnowTricks</li>
-                <li>Créez un web service exposant une API</li>
-                <li>Améliorez une application existante de ToDo & Co</li>
-            </ul>
+                <div class="card-deck">
+                    <a class="card" href="https://carte.ovh">Un site pour s'orienter en balade</a>
 
-            <h3>Mes projets perso</h3>
-
-            <div class="card-deck">
-                <a class="card" href="https://carte.ovh">Un site pour s'orienter en balade</a>
-
-                <a class="card" href="https://sansgodasses.com">Un site sur la course pieds nus</a>
+                    <a class="card" href="https://sansgodasses.com">Un site sur la course pieds nus</a>
+                </div>
             </div>
         </section>
 
