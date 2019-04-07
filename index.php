abf579ce742f01de1029705403ddaf4053c8a3bc
Nicolas <nicolas.renvoise.dev@gmail.com>
2019-04-07 08:45:57 +0100

    Add project 5 link

--- index.php
+++ index.php
@@ -1,29 +1,15 @@
-<!DOCTYPE html>
-<html lang="fr">
-    <head>
-        <meta charset="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <meta name="description" content="Imprimer du contenu web">
-        <meta name="author" content="Nicolas Renvoisé">
-
-        <title>Mes projets - Formation backend</title>
-
-        <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
-        <link href="css/style.css" rel="stylesheet" />
-    </head>
-    
-    <body>
-        <header>
-            <h1>Nicolas Renvoisé</h1>
-            <h2>Développeur backend en formation</h2>
-        </header>
-
+<?php
+ob_start();
+?>
         <section class="content">
             <div class="box">
                 <h3>La formation OpenClassrooms</h3>
 
                 <div class="card-deck">
-                    <a class="card" href="https://openclassrooms.com/fr/paths/59-developpeur-dapplication-php-symfony">Développeur d'application php/symfony</a>
+                    <a class="card" href="https://openclassrooms.com/fr/paths/59-developpeur-dapplication-php-symfony">
+                        <h5 class="card-title">Développeur d'application php/symfony</h5>
+                        <p class="card-body">Voir le contenu de la formation</p>
+                    </a>
                 </div>
             </div>
 
@@ -33,17 +19,30 @@
                 <h4>Projets réalisés</h4>
 
                 <div class="card-deck">                
-                    <a class="card" href="https://formation-oc-php.sansgodasses.com/projet-2/">Intégrez un thème Wordpress pour un client</a>
+                    <a class="card" href="https://formation-oc-php.sansgodasses.com/projet-2/">
+                        <h5 class="card-title">Intégrez un thème Wordpress pour un client</h5>
+                        <p class="card-body">Voir le site</p>
+                    </a>
 
-                    <a class="card" href="https://formation-oc-php.sansgodasses.com/projet-3/">Analysez les besoins de votre client pour son Festival de films</a>
+                    <a class="card" href="https://formation-oc-php.sansgodasses.com/projet-3/">
+                        <h5 class="card-title">Analysez les besoins de votre client pour son Festival de films</h5>
+                        <p class="card-body">Voir le site</p>
+                    </a>
 
-                    <a class="card" href="https://formation-oc-php.sansgodasses.com/projet-4/">Concevez la solution technique d'une application de restauration en ligne, Express Food</a>
+                    <a class="card" href="https://formation-oc-php.sansgodasses.com/projet-4/">
+                        <h5 class="card-title">Concevez la solution technique d'une application de restauration en ligne, Express Food</h5>
+                        <p class="card-body">Voir les diagrammes UML et le fichier .sql</p>
+                    </a>
+
+                    <a class="card" href="https://blog.sansgodasses.com/">
+                        <h5 class="card-title">Créez votre premier blog en PHP</h5>
+                        <p class="card-body">Voir le site</p>
+                    </a>
                 </div>
 
                 <h4>Projets à venir...</h4>
 
                 <ul>
-                    <li>Créez votre premier blog en PHP</li>
                     <li>Développez de A à Z le site communautaire SnowTricks</li>
                     <li>Créez un web service exposant une API</li>
                     <li>Améliorez une application existante de ToDo & Co</li>
@@ -54,22 +53,19 @@
                 <h3>Mes projets perso</h3>
 
                 <div class="card-deck">
-                    <a class="card" href="https://carte.ovh">Un site pour s'orienter en balade</a>
+                    <a class="card" href="https://carte.ovh">
+                        <h5 class="card-title">Un site pour s'orienter en balade</h5>
+                        <p class="card-body">Voir le site</p>
+                    </a>
 
-                    <a class="card" href="https://sansgodasses.com">Un site sur la course pieds nus</a>
+                    <a class="card" href="https://sansgodasses.com">
+                        <h5 class="card-title">Un site sur la course pieds nus</h5>
+                        <p class="card-body">Voir le site</p>
+                    </a>
                 </div>
             </div>
         </section>
+<?php
+$content = ob_get_clean();
 
-        <footer>
-            <ul>
-                <li>
-                    <a href="https://github.com/nicordev">GitHub</a>
-                </li>
-                <li>
-                    <a href="https://www.linkedin.com/in/nicolas-renvoise-developer/">LinkedIn</a>
-                </li>
-            </ul>
-        </footer>
-    </body>
-</html>
+require 'view/layout.php';
---
+++ view/layout.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta name="description" content="Imprimer du contenu web">
+        <meta name="author" content="Nicolas Renvoisé">
+
+        <title>Mes projets - Formation backend</title>
+
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+        <link href="css/style.css" rel="stylesheet" />
+    </head>
+    
+    <body>
+        <header>
+            <h1>Nicolas Renvoisé</h1>
+            <h2>Développeur backend en formation</h2>
+        </header>
+
+        <section class="content">
+            <?= $content ?>
+        </section>
+
+        <footer>
+            <ul>
+                <li>
+                    <a href="https://github.com/nicordev">GitHub</a>
+                </li>
+                <li>
+                    <a href="https://www.linkedin.com/in/nicolas-renvoise-developer/">LinkedIn</a>
+                </li>
+            </ul>
+        </footer>
+    </body>
+</html>
